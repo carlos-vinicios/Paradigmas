3b80b8117a18f8a4fc4ab46de254c384417e55ea
Carlos Vinicios Martins Rocha <vinicios_martins2011@hotmail.com>
2018-10-17 15:19:18 -0300

    Correções da Lista 1

--- Lista1/src/lista1/Lista1.java
+++ Lista1/src/lista1/Lista1.java
@@ -5,10 +5,6 @@ import java.util.*;
  * @author carlos_vinicios
  **/
 public class Lista1 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         
